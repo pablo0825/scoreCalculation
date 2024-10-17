b731897c68578070094a98541dad5a3e3e4fd994
pablo0825 <pablo0825@stust.edu.tw>
2024-10-17 18:08:54 +0800

    1017
    完成練習的所有功能，但有滿多功能不清楚的，而且程式碼滿亂的，有滿多重複的程式碼。
    建議之後可以自己在重新整理一遍。

--- js/app.js
+++ js/app.js
@@ -1,3 +1,477 @@
 let hero = document.querySelector(".hero");
 let slider = document.querySelector(".slider");
 let animation = document.querySelector("section.animation-wrapper");
+
+/* const time_line = new TimelineMax();
+
+time_line
+  .fromTo(hero, 1, { height: "0%" }, { height: "100%", ease: Power2.easeInOut })
+  .fromTo(
+    hero,
+    1.2,
+    { width: "80%" },
+    { width: "100%", ease: Power2.easeInOut }
+  )
+  .fromTo(
+    slider,
+    1,
+    { x: "-100%" },
+    { x: "0%", ease: Power2.easeInOut },
+    "-=1.2"
+  )
+  .fromTo(animation, 0.3, { opacity: 1 }, { opacity: 0 });
+
+setTimeout(() => {
+  animation.style.pointerEvents = "none";
+}); */
+
+//讓整個網站的enter無法使用
+window.addEventListener("keypress", (e) => {
+  if (e.key == "Enter") {
+    e.preventDefault();
+  }
+});
+
+//阻止form內部的btn交出表單
+let allbtn = document.querySelectorAll("button");
+allbtn.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+  });
+});
+
+//選擇select內的option後，改變成對應的顏色
+let allSelect = document.querySelectorAll("select");
+allSelect.forEach((select) => {
+  select.addEventListener("change", (e) => {
+    setGPA();
+    changeColor(e.target); // 傳入變更事件的目標元素
+  });
+});
+
+//改變credit後，gpa也要更新
+let credits = document.querySelectorAll(".class-credit");
+credits.forEach((credit) => {
+  credit.addEventListener("change", () => {
+    setGPA();
+  });
+});
+
+function changeColor(selectElement) {
+  // 根據 select 的 value 設置背景顏色
+  if (selectElement.value === "A" || selectElement.value === "A-") {
+    selectElement.style.backgroundColor = "#a7e394"; // 對單一元素應用樣式
+    selectElement.style.color = "#ffffff";
+  } else if (
+    selectElement.value === "B+" ||
+    selectElement.value === "B" ||
+    selectElement.value === "B-"
+  ) {
+    selectElement.style.backgroundColor = "#92e0e0"; // 對單一元素應用樣式
+    selectElement.style.color = "#ffffff";
+  } else if (
+    selectElement.value === "C+" ||
+    selectElement.value === "C" ||
+    selectElement.value === "C-"
+  ) {
+    selectElement.style.backgroundColor = "#92a6e0"; // 對單一元素應用樣式
+    selectElement.style.color = "#ffffff";
+  } else if (
+    selectElement.value === "D+" ||
+    selectElement.value === "D" ||
+    selectElement.value === "D-"
+  ) {
+    selectElement.style.backgroundColor = "#b992e0"; // 對單一元素應用樣式
+    selectElement.style.color = "#ffffff";
+  } else if (selectElement.value === "F") {
+    selectElement.style.backgroundColor = "#e092d0"; // 對單一元素應用樣式
+    selectElement.style.color = "#ffffff";
+  } else {
+    selectElement.style.backgroundColor = "#ffffff";
+    selectElement.style.color = "#000000"; // 對單一元素應用樣式
+  }
+}
+
+function convertor(grade) {
+  switch (grade) {
+    case "A":
+      return 4.0;
+    case "A-":
+      return 3.7;
+    case "B+":
+      return 3.4;
+    case "B":
+      return 3.0;
+    case "B-":
+      return 2.7;
+    case "C+":
+      return 2.4;
+    case "C":
+      return 2.0;
+    case "C-":
+      return 1.7;
+    case "D+":
+      return 1.4;
+    case "D":
+      return 1.0;
+    case "D-":
+      return 0.7;
+    case "F":
+      return 0.0;
+    default:
+      return 0;
+  }
+}
+
+function setGPA() {
+  let formLength = document.querySelectorAll("form").length;
+  let credits = document.querySelectorAll(".class-credit");
+  let select = document.querySelectorAll("select");
+  let sum = 0;
+  let creditSum = 0;
+
+  for (let i = 0; i < credits.length; i++) {
+    // 確保正確拼寫 valueAsNumber
+    if (!isNaN(credits[i].valueAsNumber)) {
+      creditSum += credits[i].valueAsNumber;
+    }
+  }
+
+  for (let i = 0; i < formLength; i++) {
+    if (!isNaN(credits[i].valueAsNumber)) {
+      sum += credits[i].valueAsNumber * convertor(select[i].value);
+    }
+  }
+
+  let result;
+  if (creditSum == 0) {
+    result = (0.0).toFixed(2);
+  } else {
+    result = (sum / creditSum).toFixed(2);
+  }
+
+  document.getElementById("result-gpa").innerText = result;
+}
+
+let addButton = document.querySelector(".plus-btn");
+addButton.addEventListener("click", () => {
+  let newForm = document.createElement("form");
+  let newDiv = document.createElement("div");
+  newDiv.classList.add("grader");
+
+  let newInput1 = document.createElement("input");
+  newInput1.setAttribute("type", "text");
+  newInput1.setAttribute("list", "opt");
+  newInput1.classList.add("class-type");
+
+  let newInput2 = document.createElement("input");
+  newInput2.setAttribute("type", "text");
+  newInput2.classList.add("class-number");
+
+  let newInput3 = document.createElement("input");
+  newInput3.setAttribute("type", "number");
+  newInput3.setAttribute("min", "0");
+  newInput3.setAttribute("max", "6");
+  newInput3.classList.add("class-credit");
+  newInput3.addEventListener("change", () => {
+    setGPA();
+  });
+
+  let newSelect = document.createElement("select");
+  newSelect.classList.add("select");
+  var opt1 = document.createElement("option");
+  opt1.setAttribute("value", "");
+  let textNode1 = document.createTextNode("");
+  opt1.appendChild(textNode1);
+  var opt2 = document.createElement("option");
+  opt2.setAttribute("value", "A");
+  let textNode2 = document.createTextNode("A");
+  opt2.appendChild(textNode2);
+  var opt3 = document.createElement("option");
+  opt3.setAttribute("value", "A-");
+  let textNode3 = document.createTextNode("A-");
+  opt3.appendChild(textNode3);
+  var opt4 = document.createElement("option");
+  opt4.setAttribute("value", "B+");
+  let textNode4 = document.createTextNode("B+");
+  opt4.appendChild(textNode4);
+  var opt5 = document.createElement("option");
+  opt5.setAttribute("value", "B");
+  let textNode5 = document.createTextNode("B");
+  opt5.appendChild(textNode5);
+  var opt6 = document.createElement("option");
+  opt6.setAttribute("value", "B-");
+  let textNode6 = document.createTextNode("B-");
+  opt6.appendChild(textNode6);
+  var opt7 = document.createElement("option");
+  opt7.setAttribute("value", "C+");
+  let textNode7 = document.createTextNode("C+");
+  opt7.appendChild(textNode7);
+  var opt8 = document.createElement("option");
+  opt8.setAttribute("value", "C");
+  let textNode8 = document.createTextNode("C");
+  opt8.appendChild(textNode8);
+  var opt9 = document.createElement("option");
+  opt9.setAttribute("value", "C-");
+  let textNode9 = document.createTextNode("C-");
+  opt9.appendChild(textNode9);
+  var opt10 = document.createElement("option");
+  opt10.setAttribute("value", "D+");
+  let textNode10 = document.createTextNode("D+");
+  opt10.appendChild(textNode10);
+  var opt11 = document.createElement("option");
+  opt11.setAttribute("value", "D");
+  let textNode11 = document.createTextNode("D");
+  opt11.appendChild(textNode11);
+  var opt12 = document.createElement("option");
+  opt12.setAttribute("value", "D-");
+  let textNode12 = document.createTextNode("D-");
+  opt12.appendChild(textNode12);
+  var opt13 = document.createElement("option");
+  opt13.setAttribute("value", "F");
+  let textNode13 = document.createTextNode("F");
+  opt13.appendChild(textNode13);
+
+  newSelect.appendChild(opt1);
+  newSelect.appendChild(opt2);
+  newSelect.appendChild(opt3);
+  newSelect.appendChild(opt4);
+  newSelect.appendChild(opt5);
+  newSelect.appendChild(opt6);
+  newSelect.appendChild(opt7);
+  newSelect.appendChild(opt8);
+  newSelect.appendChild(opt9);
+  newSelect.appendChild(opt10);
+  newSelect.appendChild(opt11);
+  newSelect.appendChild(opt12);
+  newSelect.appendChild(opt13);
+
+  newSelect.addEventListener("change", (e) => {
+    setGPA();
+    changeColor(e.target);
+  });
+
+  let newButton = document.createElement("button");
+  newButton.classList.add("trash-button");
+
+  let newItag = document.createElement("i");
+  newItag.classList.add("fas");
+  newItag.classList.add("fa-trash");
+  newButton.appendChild(newItag);
+
+  newButton.addEventListener("click", (e) => {
+    form = e.target.parentElement.parentElement;
+    e.preventDefault();
+    form.style.animation = "scaleDown 0.5s ease forwards";
+    form.addEventListener("animationend", (e) => {
+      e.target.remove();
+      setGPA();
+    });
+  });
+
+  newDiv.appendChild(newInput1);
+  newDiv.appendChild(newInput2);
+  newDiv.appendChild(newInput3);
+  newDiv.appendChild(newSelect);
+  newDiv.appendChild(newButton);
+
+  newForm.appendChild(newDiv);
+  document.querySelector(".all-inputs").appendChild(newForm);
+  newForm.style.animation = "scaleUp 0.5s ease forwards";
+});
+
+// JavaScript 程式碼
+let allTrash = document.querySelectorAll(".trash-button");
+allTrash.forEach((trash) => {
+  trash.addEventListener("click", (e) => {
+    e.target.parentElement.parentElement.classList.add("remove");
+  });
+});
+allTrash.forEach((trash) => {
+  let form = trash.parentElement.parentElement;
+  form.addEventListener("transitionend", (e) => {
+    e.target.remove();
+    setGPA();
+  });
+});
+
+//合併排序演算法
+let btn1 = document.querySelector(".sort-desecending");
+let btn2 = document.querySelector(".sort-ascending");
+
+btn1.addEventListener("click", () => {
+  handleSorting("desecending");
+});
+btn2.addEventListener("click", () => {
+  handleSorting("ascending");
+});
+
+function handleSorting(direction) {
+  let graders = document.querySelectorAll("div.grader");
+  let objectArray = [];
+
+  for (let i = 0; i < graders.length; i++) {
+    let class_name = graders[i].children[0].value;
+    let class_number = graders[i].children[1].value;
+    let class_credit = graders[i].children[2].value;
+    let class_grade = graders[i].children[3].value;
+    if (
+      !(
+        (class_name == "") &
+        (class_number == "") &
+        (class_credit == "") &
+        (class_grade == "")
+      )
+    ) {
+      let class_object = {
+        class_name,
+        class_number,
+        class_credit,
+        class_grade,
+      };
+      objectArray.push(class_object);
+    }
+  }
+
+  //把成績轉換成數字
+  for (let i = 0; i < objectArray.length; i++) {
+    objectArray[i].class_grade_number = convertor(objectArray[i].class_grade);
+  }
+
+  objectArray = mergeSort(objectArray);
+  if (direction == "desecending") {
+    objectArray = objectArray.reverse();
+  }
+
+  let allInputs = document.querySelector(".all-inputs");
+  allInputs.innerHTML = "";
+
+  for (let i = 0; i < objectArray.length; i++) {
+    allInputs.innerHTML += `<form>
+    <div class="grader">
+        <input
+        type="text"
+        placeholder="class category"
+        class="class-type"
+        list="opt"
+        value=${objectArray[i].class_name}
+        /><!--
+        --><input
+        type="text"
+        placeholder="class number"
+        class="class-number"
+        value=${objectArray[i].class_number}
+        /><!--
+        --><input
+        type="number"
+        placeholder="credits"
+        min="0"
+        max="6"
+        class="class-credit"
+        value=${objectArray[i].class_credit}
+        /><!--
+        --><select name="select" class="select">
+        <option value=""></option>
+        <option value="A">A</option>
+        <option value="A-">A-</option>
+        <option value="B+">B+</option>
+        <option value="B">B</option>
+        <option value="B-">B-</option>
+        <option value="C+">C+</option>
+        <option value="C">C</option>
+        <option value="C-">C-</option>
+        <option value="D+">D+</option>
+        <option value="D">D</option>
+        <option value="D-">D-</option>
+        <option value="F">F</option></select
+        ><!--
+        --><button class="trash-button">
+        <i class="fas fa-trash"></i>
+        </button>
+    </div>
+    </form>`;
+  }
+
+  graders = document.querySelectorAll("div.grader");
+  for (let i = 0; i < graders.length; i++) {
+    graders[i].children[3].value = objectArray[i].class_grade;
+  }
+
+  //select事件監聽
+  let allSelects = document.querySelectorAll("select");
+  allSelects.forEach((select) => {
+    changeColor(select);
+
+    select.addEventListener("change", (e) => {
+      setGPA();
+      changeColor(e.target);
+    });
+  });
+
+  //credit事件監聽
+  let allCredits = document.querySelectorAll(".class-credit");
+  allCredits.forEach((credit) => {
+    credit.addEventListener("change", (e) => {
+      setGPA();
+    });
+  });
+
+  //垃圾桶
+  let allTrash = document.querySelectorAll(".trash-button");
+  allTrash.forEach((trash) => {
+    trash.addEventListener("click", (e) => {
+      e.preventDefault();
+      e.target.parentElement.parentElement.style.animation =
+        "scaleDown 0.5s ease forwards";
+      e.target.parentElement.parentElement.addEventListener(
+        "animationend",
+        (e) => {
+          e.target.remove();
+          setGPA();
+        }
+      );
+    });
+  });
+}
+
+function merge(a1, a2) {
+  let result = [];
+  let i = 0;
+  let j = 0;
+
+  while (i < a1.length && j < a2.length) {
+    if (a1[i].class_grade_number > a2[j].class_grade_number) {
+      result.push(a2[j]);
+      j++;
+    } else {
+      result.push(a1[i]);
+      i++;
+    }
+  }
+  while (i < a1.length) {
+    result.push(a1[i]);
+    i++;
+  }
+  while (j < a2.length) {
+    result.push(a2[j]);
+    j++;
+  }
+
+  return result;
+}
+
+//遞迴，把陣列的長度切到等於1，在回傳結果
+function mergeSort(arr) {
+  if (arr.length == 0) {
+    return;
+  }
+
+  if (arr.length == 1) {
+    return arr;
+  } else {
+    let middle = Math.floor(arr.length / 2);
+    let left = arr.slice(0, middle);
+    let right = arr.slice(middle, arr.length);
+    return merge(mergeSort(left), mergeSort(right));
+  }
+}
